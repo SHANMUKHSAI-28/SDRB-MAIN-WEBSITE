be6fae9727d9dd3f6f4820fba30f2487ff478cec
SHANMUKH SAI <saishanmuk28@gmail.com>
2025-01-08 10:55:29 +0530

    layoutchanged

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,5 @@
+"use client"; // This file is now a Client Component
+
 import GlobalState from '@/context';
 import './globals.css';
 import { Inter } from 'next/font/google';
@@ -5,26 +7,10 @@ import Navbar from '@/components/Navbar';
 import Footer from '../components/Footer/Footer';
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import { Analytics } from "@vercel/analytics/react";
-import { useEffect } from 'react';
 
 const inter = Inter({ subsets: ['latin'] });
 
-export const metadata = {
-  title: 'SDRB TECHNOLOGIES | Best Home Automations Company',
-  description: 'Your Tech, Upgraded',
-};
-
 export default function RootLayout({ children }) {
-  useEffect(() => {
-    // Dynamically load Razorpay script
-    const razorpayScript = document.createElement('script');
-    razorpayScript.src = 'https://checkout.razorpay.com/v1/checkout.js';
-    razorpayScript.async = true;
-    razorpayScript.onload = () => console.log('Razorpay script loaded successfully');
-    razorpayScript.onerror = () => console.error('Failed to load Razorpay script');
-    document.body.appendChild(razorpayScript);
-  }, []);
-
   return (
     <html lang="en">
       <body className={inter.className}>
---
+++ src/app/metadata.js
@@ -0,0 +1,6 @@
+// src/app/metadata.js
+export const metadata = {
+    title: 'SDRB TECHNOLOGIES | Best Home Automations Company',
+    description: 'Your Tech, Upgraded',
+  };
+  
